1d483bdc637e4fbda7d4574fa3d4c9a3e60f0e51
Stolpmann <evan.stolpmann@hotmail.com>
2022-05-06 15:51:16 -0400

    voting added

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from random import randrange
 from . import models
 from .database import engine, SessionLocal
-from .routers import post, user, auth
+from .routers import post, user, auth, vote
 
 
 #Creates Tables in postgress
@@ -16,6 +16,7 @@ app = FastAPI()
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
 
 
 # Path Operation
