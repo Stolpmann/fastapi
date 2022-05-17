1a766cca6e0d8d9550ceaa272a20c4678b307d38
Stolpmann <evan.stolpmann@hotmail.com>
2022-05-17 14:27:52 -0400

    docker error

--- alembic/env.py
+++ alembic/env.py
@@ -5,14 +5,14 @@ from sqlalchemy import pool
 
 from alembic import context
 from app.models import Base
+from app.config import settings
 
-from app.secrets import SQL_info
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 config.set_main_option(
-    "sqlalchemy.url", SQL_info
+    "sqlalchemy.url", f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 )
 
 # Interpret the config file for Python logging.
